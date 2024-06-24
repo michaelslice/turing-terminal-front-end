4b9ee7b24e6dd11468534198c07a71fca983cf7b
michaelslice <mjsens523@gmail.com>
2024-06-24 02:47:24 -0500

    Added Skeleton Code for All Components

--- turing-terminal-front-end/src/Components/Terminal/terminal.tsx
+++ turing-terminal-front-end/src/Components/Terminal/terminal.tsx
@@ -1,13 +1,27 @@
 import { useFetcher } from "react-router-dom";
 import useDragger from "./DraggerComponent/dragger";
 import React, { useState, useEffect, useRef} from 'react'
-
 import Settings from "./SettingsComponent/settings";
 import Filings from "./FilingsComponent/filings";
+import Holders from "./HoldersComponent/holders";
+import News from "./NewsComponent/news";
+import Focus from "./FocusComponent/focus";
+import Chart from "./ChartComponent/chart";
+import Help from "./HelpComponent/help";
+import Chat from "./ChatComponent/chat";
+import QuoteMonitor from "./QuoteMonitorComponent/quotemonitor";
+import AccountManagement from "./AccountManagementComponent/accountmanage";
+import UserBiography from "./UserBiographyComponent/userbiography";
+import Description from "./DescriptionComponent/description";
+import Financials from "./FinancialsComponent/financials";
+import TimeAndSales from "./TimeAndSalesComponent/timeandsales";
+import CompanyEvents from "./CompanyEventsComponent/companyevents";
+import OptionsChain from "./OptionChainComponent/optionchain";
+import EquityScreener from "./EquityScreenerComponent/equityscreener";
+import Ipo from "./IPOComponent/ipo";
+import WorldIndices from "./WorldIndicesComponent/worldindices";
 
 import "./terminal.css"
-
-
 
 function Terminal() {
 
@@ -81,36 +95,31 @@ function Terminal() {
     })
 
     const [search, setSearch] = useState(" ");
-    const func = (e: any) => { 
+    
+    const searchValue = (e: any) => { 
         setSearch(e.target.value)
     }
 
 
     const [openSetting, setOpenSettings] = useState(false);
-
-    const openSettings = () => {
-        console.log("open settings")
-        setOpenSettings(true);
-    }
-        
-    const closeOpenSettings = () => {
-        console.log("close settings")
-        setOpenSettings(false);
-    }
-    
     const [openFilings, setOpenFilings] = useState(false);
-
-    const openFiling = () => {
-        console.log("open settings")
-        setOpenFilings(true);
-    }
-        
-    const closeOpenFilings = () => {
-        console.log("close settings")
-        setOpenFilings(false);
-    }
-
-
+    const [openHolders, setOpenHolders] = useState(false);
+    const [openNews, setOpenNews] = useState(false);
+    const [openFocus, setOpenFocus] = useState(false);
+    const [openChart, setOpenChart] = useState(false);
+    const [openHelp, setOpenHelp] = useState(false);
+    const [openChat, setOpenChat] = useState(false);
+    const [openQuoteMonitor, setOpenQuoteMonitor] = useState(false);
+    const [openAccountManagement, setOpenAccountManagement] = useState(false);
+    const [openUserBiography, setOpenUserBiography] = useState(false);
+    const [openDescription, setOpenDescription] = useState(false);
+    const [openFinancials, setOpenFinancials] = useState(false);
+    const [openTimeAndSales, setOpenTimeAndSales] = useState(false);
+    const [openCompanyEvents, setOpenCompanyEvents] = useState(false);
+    const [openOptionChain, setOpenOptionChain] = useState(false);
+    const [openEquityScreener, setOpenEquityScreener] = useState(false);
+    const [openInitialPublicOfferings, setOpenInitialPublicOfferings] = useState(false);
+    const [openWordIndices, setOpenWordIndices] = useState(false);
 
     return (
         <div className="terminal">
@@ -126,7 +135,7 @@ function Terminal() {
                             <span>Terminal</span> 
                             : 
                             <div className="search-bar-div">
-                                <input onChange={func} className="search-bar-input"></input>                          
+                                <input onChange={searchValue} className="search-bar-input"></input>                          
                                 <i></i>
                             </div>
                         }
@@ -134,121 +143,121 @@ function Terminal() {
                 </div>
 
                 <div className="middle-settings-options">
-                    {visibility.filings && <button onClick={openFiling}>
+                    {visibility.filings && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenFilings(true)}>
                         <div className="pin-commands">
                             <span>CF</span>
                         </div>
                     </button>}
 
-                    {visibility.holders && <button onClick={openSettings}>
+                    {visibility.holders && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenHolders(true)}>
                     <div className="pin-commands">
                             <span>HDS</span>
                         </div>
                     </button>}
 
-                    {visibility.news && <button onClick={openSettings}>
+                    {visibility.news && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenNews(true)}>
                         <div className="pin-commands">
                             <span>N</span>
                         </div>
                     </button>}
 
-                    {visibility.focus && <button onClick={openSettings}>
+                    {visibility.focus && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenFocus(true)}>
                         <div className="pin-commands">
                             <span>FOCUS</span>
                         </div>
                     </button>}
                      
-                    {visibility.chart && <button onClick={openSettings}>
+                    {visibility.chart && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenChart(true)}>
                         <div className="pin-commands">
                             <span>C</span>
                         </div>
                     </button>}
 
-                    {visibility.help && <button onClick={openSettings}>
+                    {visibility.help && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenHelp(true)}>
                         <div className="pin-commands">
                             <span>HELP</span>
                         </div>
                     </button>}
 
-                    {visibility.chat && <button onClick={openSettings}>
+                    {visibility.chat && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenChat(true)}>
                         <div className="pin-commands">
                             <span>CHAT</span>
                         </div>
                     </button>}
                     
-                    {visibility.quoteMonitor && <button onClick={openSettings}>
+                    {visibility.quoteMonitor && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenQuoteMonitor(true)}>
                         <div className="pin-commands">
                             <span>QM</span>
                         </div>
                     </button>}
 
-                    {visibility.accountManagement && <button onClick={openSettings}>
+                    {visibility.accountManagement && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenAccountManagement(true)}>
                         <div className="pin-commands">
                             <span>ACM</span>
                         </div>
                     </button>}
 
-                    {visibility.userBiography && <button onClick={openSettings}>
+                    {visibility.userBiography && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenUserBiography(true)}>
                         <div className="pin-commands">
                             <span>BIO</span>
                         </div>
                     </button>}
 
-                    {visibility.description && <button onClick={openSettings}>
+                    {visibility.description && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenDescription(true)}>
                         <div className="pin-commands">
                             <span>DES</span>
                         </div>
                     </button>}
 
-                    {visibility.financials && <button onClick={openSettings}>
+                    {visibility.financials && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenFinancials(true)}>
                         <div className="pin-commands">
                             <span>FA</span>
                         </div>
                     </button>}
 
-                    {visibility.settings && <button onClick={openSettings}>
+                    {visibility.settings && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenSettings(true)}>
                         <div className="pin-commands">
                             <span>PDF</span>
                         </div>
                     </button>}
 
-                    {visibility.timeAndSales && <button onClick={openSettings}>
+                    {visibility.timeAndSales && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenTimeAndSales(true)}>
                         <div className="pin-commands">
                             <span>TAS</span>
                         </div>
                     </button>}
 
-                    {visibility.companyEvents && <button onClick={openSettings}>
+                    {visibility.companyEvents && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenCompanyEvents(true)}>
                         <div className="pin-commands">
                             <span>EVT</span>
                         </div>
                     </button>}
 
-                    {visibility.optionChain && <button onClick={openSettings}>
+                    {visibility.optionChain && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenOptionChain(true)}>
                         <div className="pin-commands">
                             <span>OPT</span>
                         </div>
                     </button>}
 
-                    {visibility.equityScreener && <button onClick={openSettings}>
+                    {visibility.equityScreener && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenEquityScreener(true)}>
                         <div className="pin-commands">
                             <span>EQS</span>
                         </div>
                     </button>}
 
-                    {visibility.initialPublicOfferings && <button onClick={openSettings}>
+                    {visibility.initialPublicOfferings && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenInitialPublicOfferings(true)}>
                         <div className="pin-commands">
                             <span>IPO</span>
                         </div>
                     </button>}
                     
-                    {visibility.wordIndices && <button onClick={openSettings}>
+                    {visibility.wordIndices && <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenWordIndices(true)}>
                         <div className="pin-commands">
                             <span>WI</span>
                         </div>
-                    </button>}
+                    </button>} 
 
-                    <button onClick={openSettings}>
+                    <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setOpenSettings(true)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="pin" viewBox="0 0 16 16">
                             <path d="M9.828.722a.5.5 0 0 1 .354.146l4.95 4.95a.5.5 0 0 1 0 .707c-.48.48-1.072.588-1.503.588-.177 0-.335-.018-.46-.039l-3.134 3.134a6 6 0 0 1 .16 1.013c.046.702-.032 1.687-.72 2.375a.5.5 0 0 1-.707 0l-2.829-2.828-3.182 3.182c-.195.195-1.219.902-1.414.707s.512-1.22.707-1.414l3.182-3.182-2.828-2.829a.5.5 0 0 1 0-.707c.688-.688 1.673-.767 2.375-.72a6 6 0 0 1 1.013.16l3.134-3.133a3 3 0 0 1-.04-.461c0-.43.108-1.022.589-1.503a.5.5 0 0 1 .353-.146m.122 2.112v-.002zm0-.002v.002a.5.5 0 0 1-.122.51L6.293 6.878a.5.5 0 0 1-.511.12H5.78l-.014-.004a5 5 0 0 0-.288-.076 5 5 0 0 0-.765-.116c-.422-.028-.836.008-1.175.15l5.51 5.509c.141-.34.177-.753.149-1.175a5 5 0 0 0-.192-1.054l-.004-.013v-.001a.5.5 0 0 1 .12-.512l3.536-3.535a.5.5 0 0 1 .532-.115l.096.022c.087.017.208.034.344.034q.172.002.343-.04L9.927 2.028q-.042.172-.04.343a1.8 1.8 0 0 0 .062.46z"/>
                         </svg>
@@ -290,38 +299,104 @@ function Terminal() {
 
             { openSetting &&
                 <Settings 
-                    closeSettings={closeOpenSettings}  
+                    setOpenSettings={setOpenSettings}
                     toggleFilingsVisibility={toggleFilingsVisibility}
-                    toggleHoldersVisibility ={toggleHoldersVisibility } 
-                    toggleNewsVisibility ={toggleNewsVisibility }
-                    toggleFocusVisibility ={toggleFocusVisibility }
-                    toggleChartVisibility ={toggleChartVisibility}
-                    toggleHelpVisibility ={toggleHelpVisibility }
-                    toggleChatVisibility ={toggleChatVisibility }
+                    toggleHoldersVisibility={toggleHoldersVisibility } 
+                    toggleNewsVisibility={toggleNewsVisibility }
+                    toggleFocusVisibility={toggleFocusVisibility }
+                    toggleChartVisibility={toggleChartVisibility}
+                    toggleHelpVisibility={toggleHelpVisibility }
+                    toggleChatVisibility={toggleChatVisibility }
                     toggleQuoteMonitorVisibility={toggleQuoteMonitorVisibility}
-                    toggleAccountManagementVisibility = {toggleAccountManagementVisibility }
-                    toggleUserBiographyVisibility = {toggleUserBiographyVisibility }
-                    toggleDescriptionVisibility= {toggleDescriptionVisibility}
-                    toggleFinancialsVisibility = {toggleFinancialsVisibility }
-                    toggleSettingsVisibility = {toggleSettingsVisibility }
-                    toggleTimeAndSalesVisibility= {toggleTimeAndSalesVisibility}
+                    toggleAccountManagementVisibility={toggleAccountManagementVisibility }
+                    toggleUserBiographyVisibility={toggleUserBiographyVisibility }
+                    toggleDescriptionVisibility={toggleDescriptionVisibility}
+                    toggleFinancialsVisibility={toggleFinancialsVisibility }
+                    toggleSettingsVisibility={toggleSettingsVisibility }
+                    toggleTimeAndSalesVisibility={toggleTimeAndSalesVisibility}
                     toggleCompanyEventsVisibility={toggleCompanyEventsVisibility} 
-                    toggleOptionChainVisibility = {toggleOptionChainVisibility }
-                    toggleEquityScreenerVisibility = {toggleEquityScreenerVisibility }
-                    toggleInitialPublicOfferingsVisibility= {toggleInitialPublicOfferingsVisibility} 
-                    toggleWordIndicesVisibility = {toggleWordIndicesVisibility }
-                />}   
+                    toggleOptionChainVisibility ={toggleOptionChainVisibility }
+                    toggleEquityScreenerVisibility={toggleEquityScreenerVisibility }
+                    toggleInitialPublicOfferingsVisibility={toggleInitialPublicOfferingsVisibility} 
+                    toggleWordIndicesVisibility={toggleWordIndicesVisibility }
+            />}   
 
             {openFilings && 
                 <Filings 
-                    closeOpenFilings={closeOpenFilings}
+                    setOpenFilings={setOpenFilings} 
             />}
+            
+            {openHolders && 
+                <Holders 
+                    setOpenHolders={setOpenHolders}
+                />}
+            
+            {openNews && 
+                <News setOpenNews={setOpenNews}
+            />}
+            
+            {openFocus && 
+                <Focus 
+                    setOpenFocus={setOpenFocus} 
+            />}
+            
+            {openChart && 
+                <Chart setOpenChart={setOpenChart}
+            />}
+            
+            {openHelp && 
+                <Help setOpenHelp={setOpenHelp} 
+            />}
+            
+            {openChat && 
+                <Chat setOpenChat={setOpenChat}
+            />}
+            
+            {openQuoteMonitor && 
+                <QuoteMonitor setOpenQuoteMonitor={setOpenQuoteMonitor} 
+            />}
+            
+            {openAccountManagement && 
+                <AccountManagement setOpenAccountManagement={setOpenAccountManagement}
+             />}
+            
+            {openUserBiography && 
+                <UserBiography setOpenUserBiography={setOpenUserBiography} 
+            />}
+            
+            {openDescription && 
+                <Description setOpenDescription={setOpenDescription} 
+            />}
+            
+            {openFinancials && 
+                <Financials setOpenFinancials={setOpenFinancials} 
+            />}
+            
+            {openTimeAndSales && 
+                <TimeAndSales setOpenTimeAndSales={setOpenTimeAndSales} 
+            />}
+            
+            {openCompanyEvents && 
+                <CompanyEvents setOpenCompanyEvents={setOpenCompanyEvents} 
+            />}
+            
+            {openOptionChain && 
+                <OptionsChain setOpenOptionChain={setOpenOptionChain} 
+            />}
+            
+            {openEquityScreener && 
+                <EquityScreener setOpenEquityScreener={setOpenEquityScreener} 
+            />}
+            
+            {openInitialPublicOfferings && 
+                <Ipo setOpenInitialPublicOfferings={setOpenInitialPublicOfferings}
+            />}
+            
+            {openWordIndices && 
+                <WorldIndices setOpenWordIndices={setOpenWordIndices}
+             />}
 
-
-            </div>
-
-
-
+        </div>
 
             <div className="bottom-menu-row">
                 <button className="main-button">
