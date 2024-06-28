b5048c98d463aae4a714d628dda4e04bd055c90b
michaelslice <mjsens523@gmail.com>
2024-06-28 06:20:30 -0500

    Added Logic For Fetching Data From the Backend

--- turing-terminal-front-end/src/Components/Test/Test.tsx
+++ turing-terminal-front-end/src/Components/Test/Test.tsx
@@ -8,9 +8,33 @@ function Test() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await api.get('http://localhost:8000/api/stocklist/');
-                console.log("SUCCESS");
-                setData(response.data);
+                const response = await api.get('http://127.0.0.1:8000/test/');
+
+                const response1 = await api.get('http://127.0.0.1:8000/test/msft/');
+                console.log(response1);
+
+            // const response2 = await api.get('http://127.0.0.1:8000/test/msftholders/');
+            // console.log(response2);
+
+            // const response3 = await api.get('http://127.0.0.1:8000/test/msft/options/');
+            // console.log(response3);
+
+            // const response4 = await api.get('http://127.0.0.1:8000/test/msft/recommendations/');
+            // console.log(response4);
+
+            // const response5 = await api.get('http://127.0.0.1:8000/test/msft/actions/');
+            // console.log(response5);
+
+            // const response6 = await api.get('http://127.0.0.1:8000/test/msft/balance-sheet/');
+            // console.log(response6);
+
+            // const response7 = await api.get('http://127.0.0.1:8000/test/msft/income-statement/');
+            // console.log(response7);
+
+            // const response8 = await api.get('http://127.0.0.1:8000/test/msft/cash-flow/');
+            // console.log(response8);
+
+            //    setData(response.data);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -21,11 +45,15 @@ function Test() {
 
     return (
         <div className="test">
-            {data.map(item => (
-                <div key={item}>
-                    {item}
-                </div>
-            ))}
+            <div className='test-form'>
+                <form method="POST">
+                    <input placeholder='Ticker'></input>
+                    <input placeholder='Price'></input>
+                </form>
+                <button>
+                    <span>Send Data</span>
+                </button>
+            </div>
         </div>
     );
 }
